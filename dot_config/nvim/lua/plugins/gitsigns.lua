1ea71a4df7581b340df269948f3659456dbd874f
happyballon <357733836@qq.com>
2026-05-18 14:15:07 +0800

    add git author in lualine

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,14 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require("gitsigns").setup()
-  end,
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup({
+			current_line_blame = true,
+			current_line_blame_opts = {
+				virt_text = false,
+				delay = 500,
+				ignore_whitespace = false,
+			},
+            current_line_blame_formatter = " <author> • <author_time:%Y-%m-%d %H:%M>"
+		})
+	end,
 }
--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,40 +1,54 @@
+local function git_blame()
+	local blame = vim.b.gitsigns_blame_line
+
+	if blame == nil or blame == "" then
+		return ""
+	end
+
+	return blame
+end
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      options = {
-        theme = "tokyonight",
-        globalstatus = true,
-        component_separators = { left = "|", right = "|" },
-        section_separators = { left = "", right = "" },
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = {
-          {
-            "filename",
-            path = 1,
-            symbols = {
-              modified = " [+]",
-              readonly = " [lock]",
-              unnamed = "[No Name]",
-            },
-          },
-        },
-        lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { { "filename", path = 1 } },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
-      },
-    },
-  },
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			options = {
+				theme = "tokyonight",
+				globalstatus = true,
+				component_separators = { left = "|", right = "|" },
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+						symbols = {
+							modified = " [+]",
+							readonly = " [lock]",
+							unnamed = "[No Name]",
+						},
+					},
+				},
+				lualine_x = { {
+                    git_blame,
+                    cond = function()
+                        return vim.b.gitsigns_blame_line ~= nil and vim.b.gitsigns_blame_line ~= ""
+                    end,
+                }, "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { { "filename", path = 1 } },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+		},
+	},
 }
