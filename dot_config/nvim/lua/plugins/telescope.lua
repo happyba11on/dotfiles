366db224bc6c34266e64e0c91866e71c7407599b
happyballon <357733836@qq.com>
2026-07-24 16:54:44 +0800

    Add Telescope implementation keymap

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,9 @@ return {
         'nvim-lua/plenary.nvim',
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
+    keys = {
+        { '<leader>gi', '<cmd>Telescope lsp_implementations<cr>', desc = 'Telescope implementations' },
+    },
     config = function()
         require('telescope').setup()
         require('telescope').load_extension('fzf')
