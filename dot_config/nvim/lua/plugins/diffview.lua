2b85719857f01fb36bb28e6292657d513e5ad94e
happyballon <357733836@qq.com>
2026-08-24 02:44:03 -0700

    update nvim

---
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,5 @@
+return {
+  "sindrets/diffview.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional, for icons in the file panel
+  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+}
